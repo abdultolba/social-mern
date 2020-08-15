44df8df839f67bcc680e92207831c22cec2bbfc9
Abdulrahman Tolba <abdultolba@gmail.com>
2020-08-15 13:25:55 -0400

    🎉 Initial project setup.

---
+++ server/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+	email: String,
+	username: String,
+	password: {type: String, select: false},
+	description: {type: String, default: ''},
+	// Simulating diverse social media platform by using variety of random user pics
+	profilePic: {type: String, default: `/assets/images/avatar_default_${Math.floor((Math.random() * 2) + 0)}.png`}
+})
+
+userSchema.methods.findByUsername = (username, callback) => {
+	return this.model('User').find({ username }, callback)
+}
+
+const user = mongoose.model('user', userSchema)
+
+module.exports = user
