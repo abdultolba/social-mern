e2eb03f74a38479b1d29f59cc514f3b796318509
Abdulrahman Tolba <abdultolba@gmail.com>
2020-08-16 14:52:10 -0400

    ✨ Added pagination and posts

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,50 +1,56 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
-const Post = require('../models/Post');
+const express = require('express')
 
+const router = express.Router()
+const User = require('../models/User')
+const Post = require('../models/Post')
 
-router.get('/:username', (req, res) => {
+router.get('/:username', (req,res) => {
 	const { username } = req.params;
-	User.findOne({ username })
-		.then(user =>
-			user
-				? res.status(200).json({ code: 200, response: user })
-				: res.status(404).json({ code: 404, response: 'Error: Couldn\'t find user '}))
-		.catch(e => res.send(500).json({ error: 'Error.' }));
+
+	User.findOne({username})
+		.then(user => 
+			user 
+				? res.status(200).json({code: 200, response: user})
+				: res.status(404).json({code: 404, response: "Error: Coudln't find user."}))
+		.catch(e => res.send(500).json({error: 'Error'}));
 
 });
 
-router.get('/:username/posts', (req, res) => {
-	const { username: profile } = req.params;
+router.get('/:username/posts', (req,res) => {
+	const { username: profile } = req.params
+	const { offset = 0, quantity = 20 } = req.query
 
-	Post.find({ profile })
+	Post.find({profile})			
+		.skip(parseInt(offset))
+		.limit(parseInt(quantity))
+		.sort('-createdAt')
 		.populate('author')
 		.exec((err, posts) => {
-			if (err) res.status(500).send("There were an error")
+			if(err) return res.status(500).send("Error")
 			res.status(200).json({
 				code: 200,
 				response: posts
 			})
-		});
+		})
 })
 
-router.post('/:username/new/post', (req, res) => {
-	const { username: profile } = req.params;
-	const { message } = req.body;
-	const { _id: author } = req.user;
+router.post('/:username/new/post', (req,res) => {
+	const { username: profile } = req.params
+	const { message } = req.body
+	const { _id: author } = req.user
 
 	new Post({ author, profile, message })
 		.save()
 		.then(newPost => {
-			Post.populate(newPost, { path: 'author' }, (err, populatedPost) => {
+			Post.populate(newPost, {path: 'author'}, (err, populatedPost) => {
 				res.status(200).json({
 					code: 200,
 					response: populatedPost
 				})
-			})
+			})			
 		})
-		.catch(e => res.status(500).send("There were an error"));
+		.catch(e => res.status(500).send("There were an error"))
 })
 
-module.exports = router;
+
+module.exports = router
