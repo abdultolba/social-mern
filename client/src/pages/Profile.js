aa72e011f204c9a7d03c1feb6f19d306d6c5797d
Abdulrahman Tolba <abdultolba@gmail.com>
2020-08-18 19:14:56 -0400

    ♻️ Changed location of settings.

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,34 +1,67 @@
 
 import React, { Component } from 'react'
+import { toggleNavbar } from '../actions/app'
+import { fetchProfile, newPost, fetchPosts, restartState } from '../actions/profile'
 import BottomScrollListener from 'react-bottom-scroll-listener'
 import { connect } from 'react-redux'
 import { Link, Redirect } from 'react-router-dom'
-
-import { fetchProfile, newPost, fetchPosts, restartState } from '../actions/profile'
+import { changeImage, changeDescription } from '../actions/settings'
+import Post from '../components/Post'
+import Loading from '../components/Loading'
+import cogoToast from 'cogo-toast'
 import VerifiedBadge from '../components/VerifiedBadge'
 import DiscoverUser from '../components/DiscoverUser'
-import { toggleNavbar } from '../actions/app'
-import Loading from '../components/Loading'
-import Post from '../components/Post'
 
 import '../styles/pages/Profile.scss'
 
 class Profile extends Component {
-	constructor(props) {
+	constructor(props){
 		super(props)
+
+		this.state = {
+			descriptionEditMode: false
+		}
 
 		this.props.toggleNavbar(true)
 		this.props.fetchProfile(this.props.match.params.id)
 		this.props.fetchPosts(this.props.match.params.id)
-		this.handleNewPost = this.handleNewPost.bind(this)
+
+		this.handleNewPost = this.handleNewPost.bind(this) 
+		this.toggleDescription = this.toggleDescription.bind(this)
+		this.updateDescription = this.updateDescription.bind(this)
 	}
 
-	componentWillUnmount() {
-		this.props.restartState()
+	toggleDescription() {
+		if(this.props.user.ownProfile)
+			this.setState(() => ({
+				descriptionEditMode: !this.state.descriptionEditMode
+			}))
+	}
+
+	updateDescription(e) {
+		e.preventDefault()
+
+		const newDescription = e.target.newDescription.value
+
+		if(this.props.logged.description != newDescription) {
+  			this.props.changeDescription(newDescription)
+
+  			this.setState(() => ({
+	  			descriptionEditMode: false
+	  		}))
+		}
+  		else {
+  			cogoToast.warn('Wait! Your description looks the same...', {
+  				position: 'bottom-right'
+  			})
+  		}
+
+  		
 	}
 
 	handleNewPost(e) {
 		e.preventDefault()
+
 		this.props.newPost({
 			username: this.props.match.params.id,
 			message: e.target.message.value
@@ -38,49 +71,70 @@ class Profile extends Component {
 	}
 
 	componentDidUpdate(prevProps) {
-		if (this.props.location !== prevProps.location) {
-			this.props.restartState()
+	    if (this.props.location !== prevProps.location) {
+	    	this.props.restartState()
 			this.props.fetchProfile(this.props.match.params.id)
-			this.props.fetchPosts(this.props.match.params.id)
-		}
-	}
+			this.props.fetchPosts(this.props.match.params.id)    
+	    }
+  	}
 
-	render() {
+  	componentWillUnmount(){
+  		this.props.restartState()
+  	}
+
+	render(){
 		return (
-			<div className="container mt-5 pt-5 animated fadeIn">
+			<div className="container mt-5 pt-5 animated fadeIn">	
 				{(!this.props.user.username && !this.props.user.loading) && 
 					<Redirect to='/404' />
 				}
 				<div className="row justify-content-center">
 					<div className="col-12 col-md-10 justify-content-center d-flex">
-						<div className="card mb-3 rounded-0 animated fadeIn" style={{ "maxWidth": "540px" }}>
-							<div className="row no-gutters">
-								<div className="col-md-4">
-									<img src={this.props.user.profilePic} className="card-img rounded-0" alt="Profile picture" />
-								</div>
-								<div className="col-md-8">
-									<div className="card-body">
-										<h5 className="card-title d-inline-flex">
-											@{this.props.user.username}
-											{this.props.user.verified && <VerifiedBadge />}
-										</h5>
-										{this.props.user.ownProfile &&
-											<Link to="/settings">
-												<span className="badge badge-pill bg-brand text-white ml-2">Edit profile</span>
-											</Link>
-										}
-										<p className="card-text mb-2">{this.props.user.description}</p>
-										{/* 3.14159265 ;) */}
-										<p className="card-text">
-						        			<span className="mr-2">
-						        				314 <small className="text-muted">Followers</small>
-						        			</span>
-						        			<span>
-						        				159 <small className="text-muted">Following</small>
-						        			</span>
-						        		</p>
-									</div>
-								</div>
+						<div className="card mb-3 rounded-0 animated fadeIn" style={{"maxWidth": "540px"}}>
+						  	<div className="row no-gutters">
+						    	<div className="col-md-4">						    		
+						    		<img src={this.props.user.profilePic} className="card-img rounded-0" alt="Profile picture" />
+						    	</div>
+						    	<div className="col-md-8">
+						      		<div className="card-body">
+						        		<h5 className="card-title d-inline-flex">
+						        			@{this.props.user.username}	
+						        			{this.props.user.verified && <VerifiedBadge />}
+						        		</h5>						        		
+					        			{this.state.descriptionEditMode 
+					        				?	
+			        						<form onSubmit={this.updateDescription}>
+			        							<div className="form-group">
+			        								<textarea 
+			        									id="newDescription" 
+			        									placeholder="i like good music" 
+			        									className="form-control" 
+			        									defaultValue={this.props.user.description}>
+			        								</textarea>
+			        							</div>
+			        							<div className="form-group mb-0">
+			        								<input type="submit" value="Update" className="my-0 py-0 text-primary cursor-pointer btn btn-link" />
+			        								<button className="text-danger btn btn-link ml-2 my-0 py-0 cursor-pointer" onClick={this.toggleDescription}>Cancel</button>
+			        							</div>
+			        						</form>		
+					        				: 
+					        				<>
+					        					<p className="card-text mb-0 py-0">
+							        				{this.props.user.description} 
+							        				{this.props.user.ownProfile && 
+							        					<i className="fas fa-pencil-alt cursor-pointer ml-2 text-brand" onClick={this.toggleDescription}></i>
+							        				}	
+							        			</p>
+							        			<p className="card-text py-0 mt-2">
+							        				<span>
+							        					<small>315 Followers</small>
+							        					<small className="ml-3">159 Following</small>
+							        				</span>
+							        			</p>
+							        		</>
+					        			}						        		
+						      		</div>
+						    	</div>
 							</div>
 						</div>
 					</div>
@@ -101,7 +155,13 @@ class Profile extends Component {
 						    			<div className="col-md-12">					    				
 						        			<form onSubmit={this.handleNewPost}>
 						        				<div className="form-group">						        	
-						        					<textarea id="message" name="message" className="form-control" placeholder="So basically, i'm very smol."></textarea>
+						        					<textarea 
+						        						id="message" 
+						        						name="message" 
+						        						className="form-control" 
+						        						rows="3" 
+						        						placeholder="">
+						        					</textarea>
 						        				</div>
 						        				<div className="form-group">
 						        					<button type="submit" className="btn btn-primary float-right">Submit</button>
@@ -112,38 +172,46 @@ class Profile extends Component {
 						      	</div>						  
 							</div>
 						</div>
-					</div>
+					</div>				
 				}
 				<div className="row justify-content-center mb-5">
 					{
 						this.props.user.posts && (
-							<>
+							<>								
 								{this.props.user.posts.items.map((post, i) => (
-									<div className="col-12 col-md-10 justify-content-center d-flex  animated slideInUp" key={post.message + i}>
-										<Post {...post} />
+									<div className="col-12 col-md-10 justify-content-center d-flex animated slideInUp" key={post.message + i}>
+										<Post {...post}/>
 									</div>
 								))}
-								{this.props.user.posts.loading && <div className="col-12 d-flex justify-content-center"><Loading classes="my-3" /></div>}
+								{this.props.user.posts.loading && <div className="col-12 d-flex justify-content-center"><Loading classes="my-3"/></div>}
 							</>
-						)
+						)	
 					}
-				</div>
-				<BottomScrollListener onBottom={() => { this.props.fetchPosts(this.props.user.username) }} />
-			</		)
+				</div>	
+				<BottomScrollListener onBottom={() => {this.props.fetchPosts(this.props.user.username)}} />
+			</div>
+		)
 	}
 }
 
-const mapStateToProps = state => ({
+const stateToProps = state => ({
 	logged: state.app.logged,
-	user: state.profile
+	user: state.profile.ownProfile 
+		? {
+			...state.profile, 
+			...state.app.logged, 
+			ownProfile: true
+		} 
+		: state.profile
 })
-
-const mapDispatchToProps = dispatch => ({
+const dispatchToProps = dispatch => ({
 	toggleNavbar: value => dispatch(toggleNavbar(value)),
 	fetchProfile: value => dispatch(fetchProfile(value)),
 	newPost: value => dispatch(newPost(value)),
 	fetchPosts: value => dispatch(fetchPosts(value)),
-	restartState: () => dispatch(restartState()),
+	changeImage: binary => dispatch(changeImage(binary)),
+	changeDescription: description => dispatch(changeDescription(description)),
+	restartState: () => dispatch(restartState())	
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile)
+export default connect(stateToProps, dispatchToProps)(Profile)
