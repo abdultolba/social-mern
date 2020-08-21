1b19ff77d63f67029e12247a9b65fef9a20fe40b
Abdulrahman Tolba <abdultolba@gmail.com>
2020-08-21 10:44:03 -0400

    🎨 Added ProfilePictureModal for displaying user photo

--- server/routes/User.js
+++ server/routes/User.js
@@ -1,52 +1,53 @@
 const express = require('express')
 const multer = require('multer')
 const path = require('path')
+const Jimp = require('jimp')
 const fs = require('fs')
 
 const User = require('../models/User')
 const mongoose = require('mongoose')
 const Post = require('../models/Post')
 
-const isAuth = require('../middlewares/auth')
+const { isAuth, checkOwnsProfile } = require('../middlewares/auth')
 const router = express.Router()
 
-router.get('/:username', (req,res) => {
+router.get('/:username', (req, res) => {
 	let { username } = req.params
 
-	User.findOne({username})
-			.then(user => {
-				if(!user)
-					return res.status(404).json({code: 404, response: 'User not found'})
+	User.findOne({ username })
+		.then(user => {
+			if (!user)
+				return res.status(404).json({ code: 404, response: 'User not found' })
 
-				Promise.all([
-					Post.count({profile: username}),
-					Post.count({likedBy: { $in: username }})
-				]).then(([posts, likes]) => {
-					res.status(200).send({
-						code: 200,
-						response: {
-							posts,
-							likes,
-							...user.toObject()
-						}
-					})
+			Promise.all([
+				Post.count({ profile: username }),
+				Post.count({ likedBy: { $in: username } })
+			]).then(([posts, likes]) => {
+				res.status(200).send({
+					code: 200,
+					response: {
+						posts,
+						likes,
+						...user.toObject()
+					}
 				})
-				.catch(e => res.send(500).json({error: 'There was an error.'}))
 			})
-			.catch(e => res.send(500).json({error: 'There was an error.'}))
+				.catch(e => res.send(500).json({ error: 'There was an error.' }))
+		})
+		.catch(e => res.send(500).json({ error: 'There was an error.' }))
 })
 
-router.get('/:username/posts', (req,res) => {
+router.get('/:username/posts', (req, res) => {
 	const { username: profile } = req.params
 	const { offset = 0, quantity = 20 } = req.query
 
-	Post.find({profile})
+	Post.find({ profile })
 		.skip(parseInt(offset))
 		.limit(parseInt(quantity))
 		.sort('-createdAt')
 		.populate('author')
 		.exec((err, posts) => {
-			if(err)
+			if (err)
 				return res.status(500).send("There were an error")
 
 			res.status(200).json({
@@ -56,19 +57,18 @@ router.get('/:username/posts', (req,res) => {
 		})
 })
 
-router.get('/:id/likes', (req,res) => {
-	//Search by id to be implemented
+router.get('/:id/likes', (req, res) => {
 	const { username } = req.params
-	Post.find({likedBy: { $in: username }})
+	Post.find({ likedBy: { $in: username } })
 		.limit(2)
 		.exec((err, posts) => {
 			res.send(posts)
 		})
-		/*.then(res => res.status(200).send(res))
-		.catch(e => res.status(500).send("There were an error"))*/
+	/*.then(res => res.status(200).send(res))
+	.catch(e => res.status(500).send("There were an error"))*/
 })
 
-router.post('/:username/new/post', isAuth, (req,res) => {
+router.post('/:username/new/post', isAuth, (req, res) => {
 	const { username: profile } = req.params
 	let { message, extra = null } = req.body
 	const { _id: author } = req.user
@@ -83,7 +83,7 @@ router.post('/:username/new/post', isAuth, (req,res) => {
 	new Post({ author, profile, message, extra })
 		.save()
 		.then(newPost => {
-			Post.populate(newPost, {path: 'author'}, (err, populatedPost) => {
+			Post.populate(newPost, { path: 'author' }, (err, populatedPost) => {
 				res.status(200).json({
 					code: 200,
 					response: populatedPost
@@ -93,21 +93,21 @@ router.post('/:username/new/post', isAuth, (req,res) => {
 		.catch(e => res.status(500).send("We couldn't save your post."))
 })
 
-router.post('/:username/edit/info/description', isAuth, (req,res) => {
+router.post('/:username/edit/info/description', isAuth, (req, res) => {
 	const { username } = req.params
 	const { description } = req.body
-	if(req.user.username != username)
-		return res.status(401).json({ code: 401, response: "Unauthorized request"})
+	if (req.user.username != username)
+		return res.status(401).json({ code: 401, response: "Unauthorized request" })
 
-	if(description.length > 150)
-		return res.status(400).json({code: 400, message: "Your description can't have more than 150 characters"})
+	if (description.length > 150)
+		return res.status(400).json({ code: 400, message: "Your description may not have more than 150 characters" })
 
 	User.findOneAndUpdate({ username }, { description: description }, { new: true, useFindAndModify: false })
 		.then(updatedUser => res.status(200).json(
 			{
 				code: 200,
 				response: {
-					message: 'Descripcion cambiada con exito',
+					message: 'Description successfully updated!',
 					newDescription: updatedUser.description,
 					updatedUser
 				}
@@ -117,41 +117,50 @@ router.post('/:username/edit/info/description', isAuth, (req,res) => {
 })
 
 var storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/images/avatars')
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${req.params.username}_${Date.now()}.png`)
-  }
+	destination: (req, file, cb) => {
+		cb(null, 'public/images/avatars')
+	},
+	filename: (req, file, cb) => {
+		cb(null, `${req.params.username}_${Date.now()}.png`)
+	}
 })
 
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage }).single('newImage')
 
-router.post('/:username/edit/info/profilePicture', upload.single('newImage'), (req,res, next) => {
+router.post('/:username/edit/info/profilePicture', [isAuth, checkOwnsProfile], (req, res) => {
 	const { username } = req.params
+	let upload = multer({ storage: storage }).single('newImage')
 
-	if(!req.file)
-		res.status(500).json(
-			{
-				code: 500,
-				response: "There were an error"
-			}
-		)
+	upload(req, res, function (err) {
+		if (!req.file)
+			res.status(500).json({ code: 500, response: "There were an error" })
 
-	User.findOneAndUpdate({ username }, { profilePic: `images/avatars/${req.file.filename}` }, { new: true, useFindAndModify: false })
-		.then(updatedUser => {
-			res.status(200).json(
-				{
-					code: 200,
-					response: {
-						message: 'Foto cambiada con exito',
-						path: updatedUser.profilePic,
-						updatedUser
-					}
-				}
-			)
+		const { x, y, width, height } = JSON.parse(req.body.crop)
+
+		Jimp.read(path.resolve(req.file.destination, req.file.filename), (err, imageToCrop) => {
+			if (err) throw err
+			imageToCrop
+				.crop(x, y, width, height)
+				.resize(150, 150)
+				.quality(100)
+				.write(path.resolve(req.file.destination, req.file.filename))
 		})
-		.catch(e => res.status(500).send(e))
+
+		User.findOneAndUpdate({ username }, { profilePic: `images/avatars/${req.file.filename}` }, { new: true, useFindAndModify: false })
+			.then(updatedUser => {
+				res.status(200).json(
+					{
+						code: 200,
+						response: {
+							message: 'Photo updated successfully!',
+							path: updatedUser.profilePic,
+							updatedUser
+						}
+					}
+				)
+			})
+			.catch(e => res.status(500).send(e))
+	})
 })
 
 module.exports = router
