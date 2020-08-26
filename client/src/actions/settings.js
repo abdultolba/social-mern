98e48759c00b9e10f72dc9354298cc20ba827fe4
Abdulrahman Tolba <abdultolba@gmail.com>
2020-08-26 18:20:10 -0400

    ✨ Added default profile avi, authentication tokens, changed routes

--- client/src/actions/settings.js
+++ client/src/actions/settings.js
@@ -16,8 +16,8 @@ export const changeImage = (binary, crop) => {
 		const state = getState()
 		const { username } = state.app.logged
 		const payload = new FormData()
-		payload.append('newImage', binary)
 		payload.append('crop', JSON.stringify(crop))
+		payload.append('newImage', binary)
 
 		API.post(`user/${username}/edit/info/profilePicture`,payload, {
 			  headers: {
