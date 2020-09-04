2e9d7622fb35a1245180bed970cb045ab2aea741
Abdulrahman Tolba <abdultolba@gmail.com>
2020-09-04 10:59:15 -0400

    ✏️ Heroku fixes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const { MONGO_URI } = process.env['MONGO_URI'] || require('../config')
+const { MONGO_URI } = process.env.MONGO_URI || require('../config')
 
 const config = {
 	useNewUrlParser: true, 
