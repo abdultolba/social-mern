fc13f4fee66d4efda48672a1fd01b248ce9ffa0b
Abdulrahman Tolba <abdultolba@gmail.com>
2020-09-27 15:37:19 -0400

    🚧 Working on accessibility + minor changes

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-class Error extends Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return (
-			<div className="container mt-5 pt-5">
-				<div className="row justify-content-center">
-					<div className="col-6 text-center">
-						<h1 className="display-1">ಥ﹏ಥ</h1>
-						<p className="lead">404 Page not found.</p>
-						<Link to="/">Back to homepage</Link>
-					</div>
+const Error = () => {
+	return (
+		<div className="container mt-5 pt-5">
+			<div className="row justify-content-center">
+				<div className="col-6 text-center">
+					<h1 className="display-1">ಥ﹏ಥ</h1>
+					<p className="lead">404 Page not found.</p>
+					<Link to="/">Back to homepage</Link>
 				</div>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
 export default Error
